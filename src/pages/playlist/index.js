390b47355a4e5fc1936a21b0898a69bd6284381b
pedroamaral91 <pedro.vinicius91@gmail.com>
2019-05-25 16:40:38 +0000

    Created styles for playlist page. (Last style changes in whole application)

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,5 +1,67 @@
 import React from 'react';
 
-const Playlist = () => <h1>Hellow!</h1>;
+import { Container, Header, SongList } from './styles';
+
+import ClockIcon from '../../assets/images/clock.svg';
+import PlusIcon from '../../assets/images/plus.svg';
+
+const Playlist = () => (
+  <Container>
+    <Header>
+      <img
+        src="https://img1-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/21041/large_thumb_473-Techo-CD-Album-Cover.jpg"
+        alt="playlist"
+      />
+      <div>
+        <span>PLAYLIST</span>
+        <h1>Jazz</h1>
+        <p>3 musics</p>
+        <button type="button">PLAY</button>
+      </div>
+    </Header>
+
+    <SongList cellPadding={0} cellSpacing={0}>
+      <thead>
+        <th />
+        <th>Title</th>
+        <th>Artist</th>
+        <th>Album</th>
+        <th>
+          <img src={ClockIcon} alt="clock icon" />
+        </th>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Michael Brecker</td>
+          <td>Nothing Personal</td>
+          <td>Greatest Hits</td>
+          <td>2:21</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Michael Brecker</td>
+          <td>Nothing Personal</td>
+          <td>Greatest Hits</td>
+          <td>2:21</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Michael Brecker</td>
+          <td>Nothing Personal</td>
+          <td>Greatest Hits</td>
+          <td>2:21</td>
+        </tr>
+      </tbody>
+    </SongList>
+  </Container>
+);
 
 export default Playlist;
